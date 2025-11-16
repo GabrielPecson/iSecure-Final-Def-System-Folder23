e670bcbe486a9c058950ad72a5fcf4eae5b935e9
GabrielPecson <2022312029@pampangastateu.edu.ph>
2025-11-16 12:15:51 +0800

    Update all changes including modified files, paths, and deployment fixes

--- php/routes/Pages/home-page.php
+++ php/routes/Pages/home-page.php
@@ -25,7 +25,7 @@ if (!$token) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" type="image/png" href="php\routes\Pages\5thFighterWing-logo.png">
+    <link rel="icon" type="image/png" href="5thFighterWing-logo.png">
     <title>5th Fighter Wing</title>
     <link href="output.css" rel="stylesheet" >
     <link rel="preconnect" href="https://fonts.googleapis.com">
