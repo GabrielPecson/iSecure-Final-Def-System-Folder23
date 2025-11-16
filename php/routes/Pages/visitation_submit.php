633bc0952cea94de6172f7b2441e71c4e844c5fe
GabrielPecson <2022312029@pampangastateu.edu.ph>
2025-11-16 21:27:21 +0800

    Update all changes including modified files, paths, and deployment fixes

--- php/routes/Pages/visitation_submit.php
+++ php/routes/Pages/visitation_submit.php
@@ -1,11 +1,7 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
 ob_start();
 session_start();
 require 'db_connect.php';
-require 'encryption_key.php';
 require 'notify.php';
 require 'audit_log.php';
 
